570b66e468fd8c57dacabebe163376a5220b17a3
Julian Londoño <julianandreslodono@gmail.com>
2012-06-12 14:23:03 -0500

    ultima version
    juego tile

---
+++ data/area01_level_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="area01_level_tiles" tilewidth="32" tileheight="32">
+ <image source="area01_tileset/area01_level_tiles.png" width="640" height="241"/>
+</tileset>
